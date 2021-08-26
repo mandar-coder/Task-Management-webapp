94ee911784e13510a2529ee40e9e7e2bd897a260
mandar-coder <mandartari9109@gmail.com>
2021-08-26 13:54:14 -0200

    load existing card function added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const taskContainer =document.querySelector(".task__container");
+let golbalTaskData = [];
+
 
 const addNewcard = () => {
     //get task data
@@ -10,6 +12,13 @@ const addNewcard = () => {
         type: document.getElementById("taskType").value,
         discription: document.getElementById("taskDiscription").value,
     };
+
+    //pushdat to array
+    golbalTaskData.push(taskData);
+
+    //add data to local storage
+    localStorage.setItem("taskyCA", JSON .stringify({card:golbalTaskData}));
+
 //generate html code
 
 const newCard =`<div id=${taskData.id} class="col-md-6 col-lg-4 my-4">
@@ -46,4 +55,39 @@ document.getElementById("taskDiscription").value ="";
 return;
 };
 
+//loadexisting tasks
 
+const loadExistData = () =>{
+
+  //check local storage 
+  const getData = localStorage.getItem("taskyCA");
+  //retrive data form local storage
+  if(!getData) return;
+  const taskCard = JSON.parse(getData);
+  const golbalTaskData= taskCard.card;
+
+  golbalTaskData.map((taskData) =>{
+    //generate html page for those data
+  
+    const newCard =`<div id=${taskData.id} class="col-md-6 col-lg-4 my-4">
+    <div class="card ">
+      <div class="card-header d-flex justify-content-end gap-2">
+        <button class="btn btn-outline-info"><i class="fas fa-pencil-alt"></i></button>
+        <button class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+      </div>
+      <div class="card-body">
+        <img  class="card-img" src="${taskData.image}" alt="image">
+        <h5 class="card-title p-2">${taskData.title}</h5>
+        <p class="card-text">${taskData.discription}</p>
+       <span class="badge bg-primary">${taskData.type}</span>
+      </div>
+      <div class="card-footer">
+       <button class="btn btn-primary">open Task</button>
+      </div>
+    </div>
+    </div>`;
+  //insert in dom
+  taskContainer.insertAdjacentHTML("beforeEnd", newCard);
+ });
+ return;
+};
